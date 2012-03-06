df72d27d749d4b12fe1a1862245f2e5383b94e4b
Max Hailperin <max.hailperin@gmail.com>
2012-03-05 19:49:48 -0600

    initial skeleton of test case for Person's take and give methods

---
+++ Gack/tests/edu/gac/mcs270/gack/shared/domain/TestTransfers.java
@@ -0,0 +1,14 @@
+package edu.gac.mcs270.gack.shared.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTransfers {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
