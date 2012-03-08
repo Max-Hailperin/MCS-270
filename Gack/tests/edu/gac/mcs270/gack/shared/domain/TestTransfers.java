45a5936adb13a00441d85736fce316103df29015
mskree <mskree@oh326dl14.olin.gac.edu>
2012-03-08 12:21:50 -0600

    Started testing taking

---
+++ Gack/tests/edu/gac/mcs270/gack/shared/domain/TestTransfers.java
@@ -0,0 +1,18 @@
+package edu.gac.mcs270.gack.shared.domain;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestTransfers {
+	
+	@Test
+	public void testTakeUnowned(){
+		Place place = new Place("place");
+		Person newOwner = new Person("newOwner", place);
+		Thing thing = new Thing("thing");
+		newOwner.take(thing);
+		assertEquals(newOwner, thing.getOwner());
+		assertTrue(newOwner.getPossessions().contains(thing));
+	}
+
+}
