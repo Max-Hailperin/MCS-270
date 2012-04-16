32eaf7825a2b5dd71d6c73b9e9bec84a8ceea1cd
Max Hailperin <max.hailperin@gmail.com>
2012-04-16 18:55:53 -0500

    Require user to be logged in so that save/load world functionality can be provided with each saved world stored out with a combination of the user's email address and the player-character's name.  (For example, max@gustavus.edu:Frodo.)

--- Gack/src/edu/gac/mcs270/gack/server/PersistenceImpl.java
+++ Gack/src/edu/gac/mcs270/gack/server/PersistenceImpl.java
@@ -7,6 +7,7 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
+import com.google.apphosting.api.ApiProxy;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import edu.gac.mcs270.gack.shared.Persistence;
@@ -19,14 +20,14 @@ public class PersistenceImpl extends RemoteServiceServlet implements Persistence
 	private static class SavedPlayer {
 		@SuppressWarnings("unused")
 		@PrimaryKey
-		private String name;
+		private String emailAndName;
 
 		@Persistent(serialized = "true")
 		private Person player;
 
-		public SavedPlayer(String name, Person player) {
+		public SavedPlayer(String emailAndName, Person player) {
 			super();
-			this.name = name;
+			this.emailAndName = emailAndName;
 			this.player = player;
 		}
 
@@ -36,17 +37,33 @@ public class PersistenceImpl extends RemoteServiceServlet implements Persistence
 	}
 
 	public Person getPlayer(String name){
+		String emailAndName = getEmailAndName(name);
 		PersistenceManager persistenceManager = PMF.get().getPersistenceManager();
 		try{
-			return persistenceManager.getObjectById(SavedPlayer.class, name).getPlayer();
+			return persistenceManager.getObjectById(SavedPlayer.class, emailAndName).getPlayer();
 		} catch(JDOObjectNotFoundException e){
 			return null;
 		}
 	}
 
 	public void savePlayer(Person player){
-	    SavedPlayer sp = new SavedPlayer(player.getName(), player);
+		String emailAndName = getEmailAndName(player.getName()); 
+	    SavedPlayer sp = new SavedPlayer(emailAndName, player);
 	    PMF.get().getPersistenceManager().makePersistent(sp);
+	}
+	
+	private String getEmailAndName(String name){
+		// The test below just makes sure we didn't miss the lines in web.xml that
+		// ensure the user has to be logged in.  If this exception is thrown, web.xml
+		// needs fixing.
+		if(!ApiProxy.getCurrentEnvironment().isLoggedIn()){
+			throw new IllegalStateException("not logged in");
+		}
+		// In the same way, ApiProxy.getCurrentEnvironment().isAdmin() could be used to
+		// determine whether the currently logged in user is known to AppEngine as an
+		// administrator of this application.  I can't think of any reason to demonstrate
+		// thatin the Land of Gack.
+		return ApiProxy.getCurrentEnvironment().getEmail() + ":" + name;
 	}
 
 	public PersistenceImpl() {
